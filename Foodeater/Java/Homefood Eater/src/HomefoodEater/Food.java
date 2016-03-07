bc32fb7bcaa54cf3229a0568cf4c31e9b52df175
songhighning <l.alex.d@hotmail.com>
2016-03-06 19:15:21 -0500

    Initialization

---
+++ Foodeater/Java/Homefood Eater/src/HomefoodEater/Food.java
@@ -0,0 +1,18 @@
+package HomefoodEater;
+
+public class Food {
+	private String foodName;
+	private double foodPrice;
+	public String getFoodName() {
+		return foodName;
+	}
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+	public double getFoodPrice() {
+		return foodPrice;
+	}
+	public void setFoodPrice(double foodPrice) {
+		this.foodPrice = foodPrice;
+	}
+}
